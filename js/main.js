ae5a33931fd24256f13a305378a2e191ecf83a1e
juliangibellino <julian.gibellino@gmail.com>
2014-02-14 00:59:47 -0500

    Global header work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+var mainNavigation = {
+	init: function(){
+		var that = this;
+		this.navButton = $("#nav-button");
+		this.nav = $("#main-nav");
 
+		this.navButton.click( function(){
+			that.toggleNav();
+		});
+	},
+	toggleNav: function(){
+		this.nav.slideToggle()
+		
+	}
+};
+
+$(function(){
+	mainNavigation.init();
+});
